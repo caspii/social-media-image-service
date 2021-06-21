1abc66f807fad90c6ae6ecafcbe23a323a92132f
Caspar Wrede <caspar.wrede@gmail.com>
2021-06-21 13:55:53 +0200

    Browser instance moved outside of route to maybe speed things up?

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
+import time
+
 from flask import Flask, send_file
 from splinter import Browser
 
 app = Flask(__name__)
+browser = Browser('firefox', headless=True)
 
 
 @app.route('/')
@@ -16,16 +19,18 @@ def preview_image(token):
     Returns an image (PNG) of the given scoreboard, so that it can be shown on social media as a preview
     """
     #url = request.args.get('url', default='https://casparwre.de', type=str)
-    browser = Browser('firefox', headless=True)
-    browser.driver.set_window_size(1200, 630)
+    tic = time.perf_counter()
     url = f'https://keepthescore.co/board/{token}/'
+    #browser = Browser('chrome', headless=False, incognito=True)
+    browser.driver.set_window_size(1200, 630)
     browser.visit(url)
     app.logger.info(f'Opengraph preview image: visiting {url}')
     temp_dir = '/tmp/'
     path = f'{temp_dir}{token}'
     screenshot_path = browser.screenshot(path)
-    browser.quit()
-    app.logger.info(f'Generated social media preview image {token}')
+    #browser.quit()
+    toc = time.perf_counter()
+    app.logger.info(f'Generated image for {token}. Time={toc-tic:0.4f} secs')
     return send_file(screenshot_path, mimetype='image/png')
 
 
