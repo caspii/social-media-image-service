e298a1b5d0ea2d22b0d894227fc650d6aaff51f2
Caspar Wrede <caspar.wrede@gmail.com>
2021-06-21 08:40:15 +0200

    Trigger deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def view_landing():
-    return "Welcome to screenshot service"
+    return "Welcome to the screenshot service!"
     logging.info(f'Root url called')
 
 @app.route('/preview_image/<token>.png')
