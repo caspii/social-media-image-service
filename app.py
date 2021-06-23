8a5b39aaffaac76f1b1bb362486e13c47c4fa582
Caspar Wrede <caspar.wrede@gmail.com>
2021-06-23 09:22:22 +0200

    Specify URL in encoded filename

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
-import time
-
-from flask import Flask, send_file
+import os
+import urllib.parse
+from flask import Flask, send_file, abort
 from splinter import Browser
 
 app = Flask(__name__)
@@ -11,26 +11,38 @@ browser = Browser('firefox', headless=True)
 @app.route('/')
 def view_landing():
     return "Welcome to the screenshot service!"
-    app.logger.info(f'Root url called')
+
+
+@app.route('/image/<path:encoded_url>.png')
+def generate_image(encoded_url):
+    """
+    Returns an image (PNG) of a URL. The URL is encoded in the path of the image being requested.
+    """
+    url_to_fetch = urllib.parse.unquote_plus(encoded_url)
+    domain = os.environ.get('DOMAIN', 'https://casparwre.de')
+    if not url_to_fetch.startswith(domain):
+        app.info(f'Not allowed to generate preview for this domain: {url_to_fetch}')
+        abort(405)
+    app.logger.debug(f'Generating preview for {url_to_fetch}')
+    browser.driver.set_window_size(1200, 630)
+    browser.visit(url_to_fetch)
+    screenshot_path = '/tmp/'
+    screenshot = browser.screenshot(screenshot_path)
+    return send_file(screenshot, mimetype='image/png')
+
 
 @app.route('/<token>.png')
-def preview_image(token):
+def generate_keepthescore_image(token):
     """
-    Returns an image (PNG) of the given scoreboard, so that it can be shown on social media as a preview
+    Legacy route for keepthescore.
     """
-    #url = request.args.get('url', default='https://casparwre.de', type=str)
-    tic = time.perf_counter()
     url = f'https://keepthescore.co/board/{token}/'
-    #browser = Browser('chrome', headless=False, incognito=True)
     browser.driver.set_window_size(1200, 630)
     browser.visit(url)
-    app.logger.info(f'Opengraph preview image: visiting {url}')
+    app.logger.info(f'Preview image: visiting {url}')
     temp_dir = '/tmp/'
     path = f'{temp_dir}{token}'
     screenshot_path = browser.screenshot(path)
-    #browser.quit()
-    toc = time.perf_counter()
-    app.logger.info(f'Generated image for {token}. Time={toc-tic:0.4f} secs')
     return send_file(screenshot_path, mimetype='image/png')
 
 
